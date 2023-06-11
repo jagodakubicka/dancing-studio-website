556b83b7123e8fcf4bbde166014fa6116f22907d
Jagoda Kubicka <93054999+jagodakubicka@users.noreply.github.com>
2023-06-11 19:19:44 +0200

    remove screen width function

--- scripts/app.js
+++ scripts/app.js
@@ -2,18 +2,8 @@ const menuList = document.querySelector('.nav-menu');
 const menuBtn = document.querySelector('.nav-btn');
 const btnImg  = document.querySelector('#nav-btn');
 
-const windowWidth = window.screen
-
-function checkScreenWidth(){
- if(window.screen.width >= 450){
- menuList.classList.remove('hide-nav')
- console.log('changed')
-}
-}
-console.log(screen.width)
 const toggleMenu = () => {
  menuList.classList.toggle('hide-nav')
 }
 
 menuBtn.addEventListener('click', () => toggleMenu())
-windowWidth.addEventListener('resize', () => checkScreenWidth())
